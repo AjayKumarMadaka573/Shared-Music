075e53119c005d18f80f1952e842508a5e8f5b94
AjayKumarMadaka573 <sanjiv12p@gmail.com>
2024-07-13 18:40:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const playButton = document.getElementById('play-button');
 const pauseButton = document.getElementById('pause-button');
 const stopButton = document.getElementById('stop-button');
-
+import WebSocket, { WebSocketServer } from 'ws';
 let ws;
 let isPlaying = false;
 
